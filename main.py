2e8494dfd637fe5845bc9e85455df97d4d303604
Mahbubul Alam <mahbubulalam5676@gmail.com>
2026-04-01 12:08:01 +0600

    feat: pagination added

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import csv
-
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 url = "https://books.toscrape.com/"
-response = requests.get(url)
 
-soup = BeautifulSoup(response.content, "html.parser")
-books = soup.find_all("article", {"class": "product_pod"})
-
-# print(books)
-# for book in books:
-#     title = book.h3.a["title"]
-#     price = book.find("p", {"class": "price_color"}).text
-#     print(f"{title} - {price}")
-
-with open("books.csv", "w", newline="") as f:
+with open("books.csv", "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
-    writer.writerow(["Title", "Price","Author", "Publisher",  "URL"])
-    for book in books:
-        title = book.h3.a["title"]
-        price = book.find("p", {"class": "price_color"}).text
-        writer.writerow([title, price])
+    writer.writerow(["Title", "Price"])
+
+    while url:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        books = soup.find_all("article", class_="product_pod")
+
+        for book in books:
+            title = book.h3.a["title"]
+            price = book.find("p", class_="price_color").text
+            writer.writerow([title, price])
+
+        # Find next page
+        next_btn = soup.find("li", class_="next")
+        if next_btn:
+            next_url = next_btn.a["href"]
+            url = urljoin(url, next_url)
+        else:
+            url = None
 
 print("Done")
